f27f04423c0ac667b897823b831360f33d11ea7c
jinish08 <jinishshah08@gmail.com>
2022-06-17 11:40:46 +0530

    used math.pi in Q65

--- 53-80.py
+++ 53-80.py
@@ -166,7 +166,7 @@ import math
 #         return self.radius**2*3.14
     
 #     def perimeter(self):
-#         return 2*self.radius*3.14
+#         return 2*self.radius*math.pi
 # NewCircle = Circle(8)
 # print(NewCircle.area())
 # print(NewCircle.perimeter())
